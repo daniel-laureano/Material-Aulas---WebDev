a01788bbe5721f18a56db3271a814047297f4077
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-10-09 22:48:53 -0300

    Inicio revisao CP5 React JS

--- Aula 9 - Revisão CP/aula-revisao/src/pages/Home.jsx
+++ Aula 9 - Revisão CP/aula-revisao/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function Home() {
-  
+  const [reactRepos, setReactRepos] = useState([]);
+  const [nodeRepos, setNodeRepos] = useState([]);
+  const API = import.meta.env.VITE_GIITHUB_API
+
+  useEffect(()=> {
+    fetch(`${API}react$per_page=5`)
+    .then(res => res.json())
+    .then(data => setReactRepos(data.items))
+  }, [])
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-center mb-8">GitHub Repositories Explorer</h1>
